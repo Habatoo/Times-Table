7d3b53de75598c179324ac60632c285d4f7833ed
Habatoo <habatoo@gmail.com>
2020-09-18 17:15:09 +0700

    final commit

--- app/src/main/java/com/demo/MainActivity.java
+++ app/src/main/java/com/demo/MainActivity.java
@@ -3,12 +3,60 @@ package com.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewNumbers;
+    private SeekBar seekBar;
+
+
+    private ArrayList<Integer> numbers;
+    private int max = 20;
+    private  int min = 1;
+    private int count = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listViewNumbers = findViewById(R.id.listViewNumbers);
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(max);
+
+        numbers = new ArrayList<>();
+        final ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, numbers);
+        listViewNumbers.setAdapter(arrayAdapter);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                numbers.clear();
+                if (progress < min) {
+                    seekBar.setProgress(min);
+                }
+                for (int j = min; j <= count; j++) {
+                    numbers.add(seekBar.getProgress() * j);
+                }
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar.setProgress(10);
     }
 }
